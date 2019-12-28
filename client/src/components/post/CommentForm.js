fd8c197a92ab0763f43bb80f17ff8be2736d5050
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-28 15:16:19 +0530

    adding comments

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,17 +1,38 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 
-const CommentForm = props => {
+
+const CommentForm = ({ postId, addComment}) => {
+    const [text, setText] = useState('');
     return (
-        <div>
-            
+        <div class="post-form">
+        <div class="bg-primary p">
+          <h3>Leave a comment</h3>
         </div>
+        <form class="form my-1" onSubmit={e => { 
+            e.preventDefault();
+            addComment(postId, { text });
+            setText('');
+            }}>
+          <textarea
+            name="text"
+            cols="30"
+            rows="5"
+            placeholder="Comment..."
+            value={text}
+            onChange={e => setText(e.target.value)}
+            required
+          ></textarea>
+          <input type="submit" class="btn btn-dark my-1" value="Submit" />
+        </form>
+      </div>
     )
 }
 
 CommentForm.propTypes = {
-
+    addComment: PropTypes.func.isRequired
 }
 
-export default CommentForm;
+export default connect(null, {addComment})(CommentForm);
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,9 @@ import Spinner from "../layout/spinner";
 import { getPost } from "../../actions/post";
 import PostItem from "../posts/PostItem";
 import { Link } from "react-router-dom";
+import CommentForm from "./CommentForm";
+import CommentItem from './CommentItem'
+
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -18,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       {" "}
       <Link to='/posts' className='btn'>Back To Posts</Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map(comment => {
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        })}
+      </div>
     </Fragment>
   );
 };
